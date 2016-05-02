6b5cdaa60dcfe275fdf5f9bb3a471fc557b12105
SeriniTY320 <SeriniTY320@gmail.com>
2016-05-02 10:48:04 +0300

    first commit

---
+++ 982t-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/982t/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/982t/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/982t/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/982t/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/982t/proprietary/vendor,system/vendor)
